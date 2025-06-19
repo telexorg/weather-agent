ec805a7136cd1ee8339e18459dfc18e35707e696
Actionsdeployer <actionsdeployer@telex.im>
2025-06-19 05:45:23 +0200

    chore: update baseurl

--- main.py
+++ main.py
@@ -59,7 +59,8 @@ def read_root():
 
 @app.get("/.well-known/agent.json")
 def agent_card(request: Request):
-    current_base_url = str(request.base_url).rstrip("/")
+    external_base = request.headers.get("x-external-base-url", "")
+    current_base_url = str(request.base_url).rstrip("/") + external_base
 
     response_agent_card = RAW_AGENT_CARD_DATA.copy()
     # new_name = f"{response_agent_card['name']}{random.randint(1, 1000)}"
